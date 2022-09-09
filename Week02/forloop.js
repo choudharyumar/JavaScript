8c2596b437e761f72bcec11bae4c69d26449d3fe
choudharyumar <choudharyumar948@gmail.com>
2022-09-09 16:21:36 +0500

    Create forloop.js

---
+++ Week02/forloop.js
@@ -0,0 +1,42 @@
+// let obj = {
+//         name: "Rohan Das",
+//         age: 78,
+//         type: "Dangerous Programmer",
+//         os: "Ubuntu"
+//     }
+//     for(let key in obj){
+//         console.log(obj [key])
+//     }
+
+
+
+
+// let obj={
+//     name:"umar",
+//     age:45,
+//     Os:"ubuntu",
+//     work:"danger"
+// }
+// for(let key in obj)
+// {
+//     console.log(obj[key]);
+// }
+
+
+
+// let arr=[1,2,3,4];
+// arr.forEach(function(element)
+// {
+//     console.log(element);
+// });
+
+// let i=0;
+// for( i=0;i<4;i++)
+// {
+//     console.log(i);
+// }
+// console.log(i);
+
+
+let i=10;
+console.log(i);
