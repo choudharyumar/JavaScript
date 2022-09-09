5a278ab1ff53372efc928ee69bc9644af544044b
choudharyumar <choudharyumar948@gmail.com>
2022-09-09 16:21:33 +0500

    Create breakloop.js

---
+++ Week02/breakloop.js
@@ -0,0 +1,33 @@
+// let i=10;
+// console.log(i);
+
+
+// //break the loop;
+// let i=10;
+// for(i;i<15;i++)
+// {
+//     console.log(i);
+//     if(i===13)
+//     {
+//         break;
+//     }
+// }
+// console.log('love the time');
+
+// continue the loop;
+
+// let i=10;
+//  for(i;i<15;i++)
+//  {
+// if(i===13)
+//      {
+//          continue;// at this time it continue is udsed to skip the iteration and than again continue the line
+//      }
+//      console.log(i+2);
+
+//  }
+//  console.log('love the time');
+
+
+ 
+
