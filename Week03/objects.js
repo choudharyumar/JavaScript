4968adc845b668ddf15daac131534e0df545ef21
choudharyumar <choudharyumar948@gmail.com>
2022-09-11 16:36:11 +0500

    Create objects.js

---
+++ Week03/objects.js
@@ -0,0 +1,107 @@
+//let user= new Object();
+// let user={
+//     name:'john',
+//     age:23
+// };
+// delete user.age;
+
+// console.log(user.name);
+// user.isadmin=true;
+// delete user.age;
+// console.log(user.age);
+// console.log(user.isadmin)
+
+// let library={
+//     books:'english',
+//     admin:'ali',
+//     password:1234,
+//     "all is well":true,
+
+// };
+// library.isHelper=true;
+
+// console.log(library.books)
+// delete library.password;  // this is use to delete the key names
+// console.log(library.admin)
+// delete library["all is well"];
+// console.log(library.password)
+// console.log(library["all is well"])
+// console.log(library.isHelper)
+
+
+// simple program
+// let user={
+//     name:'ali',
+//     age:29,
+//     classNo:5,
+//     student:'good'
+// };
+// user.isHelper=true;
+
+// let key=prompt('what do you want ?','name');
+// //delete user[key];  // it is used to remove the identifier 'key'
+
+// console.log(user[key])
+// delete user.isHelper;
+// console.log(user.isHelper)
+
+
+
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
+  
+//   let key = "name";
+//   console.log( user[key] )
+//   console.log( user.age )
+
+
+// computed properties"
+// let fruit=prompt('which fruit do you want?','apple');
+
+// let bag ={
+//     [fruit]:5
+
+// };
+// console.log(bag.apple)
+
+
+
+// property value short hand
+// function x(name,age){
+//     return{
+//         name,
+//         age
+//     };
+// }
+// let y=x('ali',23);
+// console.log(y.name)
+// console.log(y.age)
+
+
+// property name limitations:
+
+let obj={
+    for:1,
+    return:2,
+    let:3,
+    loop:4
+};
+console.log(obj.for+obj.return+obj.let+obj.loop)
+let x={
+    0:'test'
+};
+console.log(x[0])
+
+
+
+
+
+
+
+
+
+
+
+
