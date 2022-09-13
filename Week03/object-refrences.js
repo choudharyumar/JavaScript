f0691ce5e6dcea6f0cc8cfdb577c09d71deaf81d
choudharyumar <choudharyumar948@gmail.com>
2022-09-13 21:47:43 +0500

    Create object-refrences.js

---
+++ Week03/object-refrences.js
@@ -0,0 +1,41 @@
+// let user={
+//     name:'ali',
+//     age:34
+// }
+// let admin=user;
+// admin.name='khizar';
+// console.log(admin.name);
+// console.log(user.age);
+// //user.name='ali';
+// console.log(user.name);
+
+
+// comarision  by refrences 
+// let a={
+//     name:'ali'
+// }
+// let b=a;// in this format we can assign the value of b to a
+// console.log(b===a)
+
+
+// let a={
+//     name:'umar'
+// };
+// let b={
+//     name:'khizar'
+// };
+// console.log(b===a)
+
+
+
+const obj={
+    name:'khzar',
+    age:34
+}
+
+obj.name='ALI';
+console.log(obj.name)
+console.log(obj.age)
+
+
+
