25d70e027cdb63d2942931eb25d1fa49b2eed123
choudharyumar <choudharyumar948@gmail.com>
2022-09-14 11:55:32 +0500

    Update object-refrences.js

--- Week03/object-refrences.js
+++ Week03/object-refrences.js
@@ -28,14 +28,94 @@
 
 
 
-const obj={
-    name:'khzar',
-    age:34
-}
+// const obj={
+//     name:'khzar',
+//     age:34
+// }
 
-obj.name='ALI';
-console.log(obj.name)
-console.log(obj.age)
+// obj.name='ALI';
+// console.log(obj.name)
+// console.log(obj.age)
+
+// cloning and merge object design
+// let x={
+//     name:'umar',
+//     age:43
+// }
+// let y={
+
+// }
+// for (let key in x)
+// {
+//     y[key]=x[key];
+// }
+//  //y.name='khizar';
+// console.log(y.name)
+// console.log(x.name)
+// console.log(x.age)
+// console.log(y.age)
+// y.name='khizar';
+// console.log(y.name)
+
+
+// object assign
+// let x={
+//     name:'kh',
+//     age:34
+// }
+// let y={
+//     'hello world':true
+// }
+// let z={
+//     'hello data':true
+// }
+// Object.assign(x,y,z);
+// console.log(x.name)
+// console.log(x['hello world'])
+// console.log(x['hello data'])
+// Object.assign(x,{name:'ali'});// use to change the property of a object
+// console.log(x.name)
+
+
+// nested clonning
+// let x={
+//     height:50,
+//     length:43,
+
+//  y:{
+//     width:45
+// }
+// };
+
+// let clone=Object.assign({},x);
+// //console.log( x.y === clone.y );
+// //x.y.width = 60;
+// //Object.assign(x,y);
+// console.log(clone.y.width)
+// x.y.width = 60;
+// console.log(clone.y.width)
+
+
+
+// stuructured clone
+let x={
+    height:50,
+    length:43,
+
+ y:{
+    width:456
+}
+};
+
+let clone = structuredClone(x);
+x.y.width=45
+console.log(clone.y.width)
+
+
+
+
+
+
 
 
 
