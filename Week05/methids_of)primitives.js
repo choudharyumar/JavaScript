b15a51e087983dcfb5ffb722fc557c1fffc30d20
choudharyumar <choudharyumar948@gmail.com>
2022-09-25 18:44:19 +0500

    Create methids_of)primitives.js

---
+++ Week05/methids_of)primitives.js
@@ -0,0 +1,46 @@
+// let obj={
+//     name:'umar',
+//   sayhi(){
+//         console.log('hy')
+//     }
+// }
+// obj.sayhi();
+// console.log(obj.name)
+
+
+
+// a primitive as an object
+// str.touppercase()
+
+// let str='hello g kivy';
+// console.log(str.toUpperCase());//HELLO G KIVY
+
+// let a=1.3456;
+// console.log(a.toFixed(2))//1.35
+
+
+// let str = "Hello";
+
+// str.test = 5;
+
+// console.log(str.test);// undefined
+
+
+
+
+
+
+// let zero = new Number(0);
+
+// if (zero) { // zero is true, because it's an object
+//   console.log( "zero is truthy!?!" );
+// }
+
+
+// console.log( typeof 0 ); // "number"
+
+// console.log( typeof new Number(0) );// object
+
+
+let num=Number('hall g ki krdy');
+console.log(typeof num)
