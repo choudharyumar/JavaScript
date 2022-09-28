e350eaf138726ea6c4e83d6ec020ee3082829842
choudharyumar <choudharyumar948@gmail.com>
2022-09-29 00:02:39 +0500

    Update numbers.js

--- Week05/numbers.js
+++ Week05/numbers.js
@@ -44,17 +44,33 @@
 
 // console.log(0.1+0.2)//0.30000000000000004
 
-let a=0.1;
-let b=0.2;
-let sum=a+b;
+// let a=0.1;
+// let b=0.2;
+// let sum=a+b;
 
-console.log(sum)//0.30000000000000004
+// console.log(sum)//0.30000000000000004
 
-console.log(sum.toFixed(2))//0.30
-console.log(+sum.toFixed(2))//0.3
+// console.log(sum.toFixed(2))//0.30
+// console.log(+sum.toFixed(2))//0.3
 
 
-//another solution for some cases but not for all
-console.log( (0.1 * 10 + 0.2 * 10) / 10 ); // 0.3
-console.log( (0.28 * 100 + 0.14 * 100) / 100); // 0.4200000000000001
+// //another solution for some cases but not for all
+// console.log( (0.1 * 10 + 0.2 * 10) / 10 ); // 0.3
+// console.log( (0.28 * 100 + 0.14 * 100) / 100); // 0.4200000000000001
 
+///Tests: isFinite and isNaN:
+// console.log(isNaN(NaN))//true
+// console.log(isNaN('dshergr  '))//true
+// console.log(isNaN('1234'))//false
+// console.log(NaN===NaN)//false
+
+
+// console.log( isFinite("15") ); // true
+// console.log( isFinite("str") ); // false, because a special value: NaN
+// console.log( isFinite(Infinity) ); // false, because a special value: Infinity
+// console.log( isFinite(1234) ); // true
+
+
+
+let num=+prompt('enter a number','');
+console.log(isFinite(num))
