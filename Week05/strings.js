e0c8f79834150dc2730c7c4cceffb719fffcfeab
choudharyumar <choudharyumar948@gmail.com>
2022-10-02 18:54:25 +0500

    Create strings.js

---
+++ Week05/strings.js
@@ -0,0 +1,19 @@
+//sum of two numbers by using backticks
+// function sum(a,b){
+//     let num1=+prompt('the first number is ',a);
+//     let num2=+prompt('the first number is ',b);
+
+//     return num1+num2;
+
+// }
+// console.log(`the sum is  : ${sum()}`)
+
+
+
+// use of back ticks
+let ru=`hun
+gin
+al`;
+console.log(ru)
+
+
