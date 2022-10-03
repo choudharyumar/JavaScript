5268021f8cc8fd72ba4644965f9ccbe4a5c26502
choudharyumar <choudharyumar948@gmail.com>
2022-10-04 00:19:39 +0500

    Update strings.js

--- Week05/strings.js
+++ Week05/strings.js
@@ -11,9 +11,22 @@
 
 
 // use of back ticks
-let ru=`hun
-gin
-al`;
-console.log(ru)
+// let ru=`hun
+// gin
+// al`;
+// console.log(ru)
 
 
+
+/// special characters by using n:
+// let list='guest\n ali\n haji\n';
+// console.log(list);
+
+// let list1='haji\numar';
+// let list2=`haji
+// umar`;
+// console.log(list1===list2)
+
+console.log('here is ch\'umar')
+console.log( `My\n`.length ); // 3
+console.log('haji'.length);
