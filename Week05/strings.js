2128a684ebf49477de20ded516b2d39da52299eb
choudharyumar <choudharyumar948@gmail.com>
2022-10-05 21:03:13 +0500

    Update strings.js

--- Week05/strings.js
+++ Week05/strings.js
@@ -27,10 +27,61 @@
 // umar`;
 // console.log(list1===list2)
 
-console.log('here is ch\'umar')
-console.log( `My\n`.length ); // 3
-console.log('haji'.length);//4
+// console.log('here is ch\'umar')
+// console.log( `My\n`.length ); // 3
+// console.log('haji'.length);//4
+
+///Accessing characters:
+
+// let str= 'hy g sunoa'; // starting from zero
+// console.log(str.at(9))//a
+// console.log(str.at(-1))//a
+// console.log(str.at(-4))//u
+// console.log(str.at(3))//g
+// console.log(str[-3])// undefined
+
+//another method
+// console.log(str[3])//g//this method is not app;icable for (-)
 
 
-let i=20;
-console.log(i)
+// for (let char of 'hello')
+// {
+//     console.log(char);
+// }
+
+
+
+//Strings are immutable:
+
+// let str='Hi';
+// str[0]='h';
+// console.log(str[0])// does not change
+
+// let str1='Hi';
+// str1='h'+str1[1];
+// console.log(str1)// hi
+
+
+// let str2='hy g ali';
+// str2='al'+str2[1];
+// console.log(str2)// aly
+
+// let str2='hy g ali';
+// str2='al'+'gi'+str2[1];//algi
+// console.log(str2)
+
+
+
+///Changing the case:
+console.log('HOW ARE YOU'.toLocaleLowerCase())//how are you
+console.log('how are you'.toLocaleUpperCase())//HW ARE YOU
+console.log('HOW ARE YOU'[4].toLocaleLowerCase())//a
+console.log('how are you'.toLocaleUpperCase())//A
+
+
+
+
+
+
+
+
