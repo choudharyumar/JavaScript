3474c67df7459ae5c9df168a978656e5f57c41f4
choudharyumar <choudharyumar948@gmail.com>
2022-10-09 23:08:38 +0500

    Update strings.js

--- Week05/strings.js
+++ Week05/strings.js
@@ -79,20 +79,124 @@
 // console.log('how are you'.toLocaleUpperCase())//A
 
 
+
+
 ///str.indexOf:
+
+
 // let str='widget';
 // console.log(str.indexOf('widget'))
 // console.log(str.indexOf('WIDGET'))
 // console.log(str.indexOf('id'))
 
 
-let stri='helo how are you are';
-console.log(stri.indexOf('are',12))
-console.log(stri.indexOf('are'))
+// let stri='helo how are you are';
+// console.log(stri.indexOf('are',12))
+// console.log(stri.indexOf('are'))
+
+
+
 // another program:
-// let str=' helo how are you you are the one and here are you';
+// let str=' helo how are you, you are one';
+// let target='are';
+// let pos=-1;
+// while((pos=str.indexOf(target,pos+1))!=-1){
+//     console.log(pos)
+// }
+
+// let str = "As sly as a fox, as strong as an ox";
+// let target = "as";
+
+// let pos = -1;
+// while ((pos = str.indexOf(target, pos + 1)) != -1) {
+//   alert( pos );
+// }
 
 
+
+
+// let str='helo g kasy ho jnab';
+// if(str.indexOf('jnab')!=-1){
+//     console.log('we find it')
+// }
+
+
+/////    includes, startsWith, endsWith:
+
+
+// console.log('how are you?'.includes('you'))
+// console.log('how are you?'.includes('yes'))
+
+// console.log('umar hayat'.includes('mar'))
+// console.log('umar hayat'.includes('mar',2))//F
+// console.log('umar hayat'.includes('mar',1))//T
+
+
+
+// console.log('helo jnab'.startsWith('hel'))
+// console.log('helo jnab'.endsWith('nab'))
+
+
+
+///Getting a substring
+// let str='heavy raining';
+// console.log(str.slice(2))
+// console.log(str.slice(2,6))
+// console.log(str.slice(-4,-1))
+
+
+// console.log(str.substring(2,6))
+// console.log(str.substring(6,2))
+// console.log(str.substring(-5,-1))
+
+
+// console.log(str.substr(2,6))
+// console.log(str.substr(-5,3))
+
+
+
+///Comparing strings
+
+
+// console.log('z'>'Z')
+// console.log('o****bchg'>'bsaisc')
+
+// console.log( "Z".codePointAt(0) ); // 90
+// console.log( "z".codePointAt(0) ); // 122
+// console.log( "z".codePointAt(0).toString(16) );//7a
+
+// console.log( String.fromCodePoint(90) ); // Z
+// console.log( String.fromCodePoint(0x5a) )
+
+
+/// program for all allphabets
+
+// let str='';
+// for(let i=0;i<=220;i++)
+// {
+//     str +=String.fromCodePoint(i);
+// }
+// console.log(str)
+
+
+///Write a function checkSpam(str) that returns true if str contains ‘viagra’ or ‘XXX’, otherwise false.
+
+// function checkSpam(str)
+// {
+//     let lowerstr= str.toLowerCase();
+//     return lowerstr.includes('vaigra')||lowerstr.includes('xxxx');
+// }
+// console.log(checkSpam('buy vaigra now'))
+// console.log(checkSpam('free xxxxx'))
+// console.log(checkSpam('this is innocent rabbit'))
+
+
+function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+      str.slice(0, maxlength - 1) + '…' : str;
+  }
+
+  console.log(truncate("What I'd like to tell on this topic is:", 20));
 
 
 
