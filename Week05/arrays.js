7a677c703c32c6d9d39349b1fe4980bed799ea8f
choudharyumar <choudharyumar948@gmail.com>
2022-10-10 21:57:40 +0500

    Create arrays.js

---
+++ Week05/arrays.js
@@ -0,0 +1,38 @@
+/// declaration of array:
+// let fruit=['orange','red','green'];
+// console.log(fruit[0])
+// console.log(fruit[1])
+// console.log(fruit[2])
+
+// now we can replace an element
+// fruit[1]='pear';
+// fruit[2]='apple';
+// console.log(fruit[0])
+// console.log(fruit[1])
+// console.log(fruit[2])
+// console.log(fruit.length)
+
+
+/// array can store different type  of elements
+
+// let arr=['apple',{name:'umar'},true,function (){console.log('hello')}];
+// console.log(arr[0])
+// console.log(arr[1].name)
+// arr[3]();
+
+
+///trailing comma;
+// let fruits = [
+//     "Apple",
+//     "Orange",
+//     "Plum"
+//   ];
+//   console.log(fruits[2])
+
+
+  //Get last elements with “at”
+
+  let fruit=['apple','juice','banana'];
+  console.log(fruit[fruit.length-1])
+// in shorter form we can use this
+console.log(fruit.at(-1))
