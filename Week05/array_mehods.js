d5437752e1bfafa929a58bcae393a86e8b7e026d
choudharyumar <choudharyumar948@gmail.com>
2022-10-13 00:18:43 +0500

    Create array_mehods.js

---
+++ Week05/array_mehods.js
@@ -0,0 +1,15 @@
+/// slice in array
+
+// let arr=[1,2,3,4,5]
+// console.log(arr.slice(2))
+// console.log(arr.slice(1,4))//starts from position 1 and end on position 3 not include positwion 4
+// console.log(arr.slice(-5))
+// console.log(arr.slice(-3))//starts fromnposition -3 and continues to end
+
+
+
+///concat in arrAy:
+let arr=[1,2]
+console.log(arr.concat([3,4],[5,6]))
+console.log(arr)
+
