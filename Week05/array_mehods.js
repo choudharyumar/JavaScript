f0acdde2129f58d3f852a5f24baa9079fa86fec6
choudharyumar <choudharyumar948@gmail.com>
2022-10-14 12:31:18 +0500

    Update array_mehods.js

--- Week05/array_mehods.js
+++ Week05/array_mehods.js
@@ -87,18 +87,117 @@
 
 // find and findIndex/findLastIndex
 
-let users=[
-{id:1,name:'john'},
-{'id':2,'name':'ali'},
+// let users=[
+// {id:1,name:'john'},
+// {'id':2,'name':'ali'},
 
-{'id':3,'name':'aly'},
-{'id':4,'name':'john'},
+// {'id':3,'name':'aly'},
+// {'id':4,'name':'john'},
 
-]
-let user=users.find(item=>item.id==3)
-console.log(user.name)//aly
- console.log(users.findIndex(user=>user.name=='john'))
- console.log(users.findLastIndex(user=>user.name=='john'))
+// ]
+// let user=users.find(item=>item.id==3)
+// console.log(user.name)//aly
+// console.log(users.findIndex(user=>user.name=='john'))
+// console.log(users.findLastIndex(user=>user.name=='john'))
 
+
+
+//filter
+
+// let users=[
+//     {id:1,name:'john'},
+//     {'id':2,'name':'ali'},
+    
+//     {'id':3,'name':'aly'},
+//     {'id':4,'name':'john'},
+    
+//     ]
+// let user=users.filter(item=>item.id<10)
+// console.log(user[1])
+// console.log(user.length)
+
+
+
+///// transform an array
+
+
+// map
+
+// let arr=['umar','khizar','ali'];
+// let lengths=arr.map(item=>item.length)
+// console.log(lengths)
+//  lengths=arr.map(item=>item.length<4)
+// console.log(lengths)
+
+
+
+//sort()
+
+// let arr=[1,2,3,11,21,33,15];
+// arr.sort();
+// console.log(arr)
+
+
+// function compareNumeric(a, b) {
+//     if (a > b) return 1;
+//     if (a == b) return 0;
+//     if (a < b) return -1;
+//   }
+  
+//   console.log(compareNumeric(2,4))//-1
+
+//   let arr = [ 1, 2, 15 ];
+  
+//   arr.sort(compareNumeric);
+  
+//   console.log(arr); 
+
+
+// best sorting algorithm
+
+
+//   let arr=[1,2,3,11,21,33,15];
+//   arr.sort();
+//   console.log(arr)//[1, 11, 15, 2, 21, 3, 33]
+//   let arr1=arr.sort((a,b) => a-b)
+//   console.log(arr)//[1, 2, 3, 11, 15, 21, 33]
+
+
+
+// loal comapare
+
+// let arr=['umar','Österreich','khizar','dilber']
+// console.log(arr.sort((a,b)=>a>b?1:-1))
+// console.log(arr.sort((a,b)=>a.localeCompare (b)))
+
+// reverse
+// let arr=[1,2,3,4,5];
+// arr.reverse();
+// console.log(arr)
+
+
+// split and join
+
+// let names='ali,umar,njia'
+// let arr=names.split(',');
+// for(let name of arr){
+//     console.log(`a message to ${name}`)// message to all name
+// }
+
+
+// let arr1='ali,umar,njia'.split(',',2);// show two names
+// console.log(arr1)
+
+
+
+// split
+let arr2='haji';
+console.log(arr2.split(''))//h,a,j,i
+
+
+//join
+let arr3=['haji','mukhtar','dil'];
+let str=arr3.join(',')
+console.log(str)
 
 
