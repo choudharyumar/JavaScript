c01e9b0bba38e9f2e461384acacef071c47ae2dd
choudharyumar <choudharyumar948@gmail.com>
2022-10-20 22:01:01 +0500

    Create map_and_set.js

---
+++ Week05/map_and_set.js
@@ -0,0 +1,49 @@
+// simple program
+
+// let map=new Map();
+// map.set(1,'num1');
+// map.set(2,'str');
+// map.set(true,'boolean');
+// console.log(map.get(1))
+// console.log(map.get(2))
+// console.log(map.get(true))
+// console.log(map.size)
+
+
+
+//Map can also use objects as keys.
+
+// let obj=
+// {name1:'umar',
+// name2:'khizar'};
+
+// let obj1=
+// {name1:'uma',
+// name2:'khizar'};
+
+// let visits=new Map()
+
+// visits.set(obj,12)
+// visits.set(obj1,121)
+
+// console.log(visits.get(obj))
+// console.log(visits.get(obj1))
+
+// console.log(obj.name1)
+
+
+let obj1={name:'umar'}
+let obj2={name:'khizar'}
+let visitsobj3={}
+visitsobj3[obj1]=123;
+visitsobj3[obj2]=1234;
+console.log(visitsobj3["[object Object]"])
+
+
+
+
+
+
+
+
+
