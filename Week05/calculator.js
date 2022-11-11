9c9711189491efc0278a798c3f8ca36e16e8f539
choudharyumar <choudharyumar948@gmail.com>
2022-11-11 21:01:30 +0500

    calculator

--- Week05/calculator.js
+++ Week05/calculator.js
@@ -23,30 +23,49 @@
 // let d=calculator();
 // console.log(d);
 
-let calculator={
-    read(){
-        this.a=+prompt('put hte value of a  :','');
-        this.b=+prompt('put hte value of b  :','');
-    },
+// let calculator={
+//     read(){
+//         this.a=+prompt('put hte value of a  :','');
+//         this.b=+prompt('put hte value of b  :','');
+//     },
 
-    sum(){
-        return this.a +this.b;
-    },
-    diff(){
-        return this.a -this.b;
-    },
-    pro(){
-        return this.a *this.b;
-    },
-    div(){
-        return this.a /this.b;
-    },
-}
+//     sum(){
+//         return this.a +this.b;
+//     },
+//     diff(){
+//         return this.a -this.b;
+//     },
+//     pro(){
+//         return this.a *this.b;
+//     },
+//     div(){
+//         return this.a /this.b;
+//     },
+// }
   
-calculator.read();
-console.log('the sum of value is :',calculator.sum())
-console.log('the diff of value is :',calculator.diff())
-console.log('the pro of value is :',calculator.pro())
-console.log('the div of value is :',calculator.div())
+// calculator.read();
+// console.log('the sum of value is :',calculator.sum())
+// console.log('the diff of value is :',calculator.diff())
+// console.log('the pro of value is :',calculator.pro())
+// console.log('the div of value is :',calculator.div())
 
+
+let arr=[11,14,16,19]
+let findex=arr[0]
+let lindex=arr[arr.length-1];
+let arr2=[]
+ for(let i=findex;i<=lindex;i++)
+{
+    if(arr.indexOf(i)==-1)
+    {
+        
+    
+    arr2.push(i);
+    }
+}
+console.log(arr2)
+
+
+let questions=['a)  what is your name?,  b)what isyour age?']
+console.log('helo how are you')
 
---
+++ Week05/index.js
@@ -0,0 +1,5 @@
+import PromptSync from "prompt-sync";
+let prompt=PromptSync()
+
+prompt('what is your name')
+prompt('what is your age?')
