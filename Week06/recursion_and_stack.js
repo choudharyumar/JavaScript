1523a68764fc560a176aed46a695c3e2889b783a
choudharyumar <choudharyumar948@gmail.com>
2022-12-01 23:23:50 +0500

    Create recursion_and_stack.js

---
+++ Week06/recursion_and_stack.js
@@ -0,0 +1,29 @@
+// two way of thinking:
+
+
+// Iterative thinking:
+
+// program of power:
+// function pow(x,n){
+//     let result=1;
+//     for(let i=0;i<n;i++){
+//          result*=x;
+//     }
+//     return result;
+// }
+// console.log(pow(2,3))
+
+
+// recursive thinking:
+
+
+function pow(x,n){
+    if(n==1){
+        return x;
+    }
+    else{
+        return x* pow(x,n-1)
+    }
+}
+console.log(pow(2,4))
+
