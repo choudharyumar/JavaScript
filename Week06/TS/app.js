b8772c0dd75c0ce0aecc4c2da3966941105036f4
choudharyumar <choudharyumar948@gmail.com>
2022-12-03 22:52:54 +0500

    Create app.js

---
+++ Week06/TS/app.js
@@ -0,0 +1,17 @@
+var myage;
+myage = 16; //narrowing
+console.log(myage);
+myage = 'dont know';
+console.log(myage);
+var newage = Math.random() > 0.6 ? 'khan' : 30;
+if (newage === 'khan') {
+    console.log(newage.toUpperCase());
+}
+var a = 5;
+console.log(typeof a);
+var age;
+age = 90;
+age = 'died';
+age = 'unknown';
+age = 'living';
+console.log(age);
