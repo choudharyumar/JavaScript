e98f5cf3f678b7ed05bcefe2d6918acdf2f804d3
choudharyumar <choudharyumar948@gmail.com>
2022-12-05 00:45:08 +0500

    Create variableScope_Closure.js

---
+++ Week06/variableScope_Closure.js
@@ -0,0 +1,31 @@
+// Code blocks:
+{
+let mess='heloo'
+console.log(mess)
+
+}
+{
+    let mess='heloo'
+    console.log(mess)  // here is the discription of calling inside block than see the results if the value of both variables are same and if we declare outside of the block than there is a error
+    
+    }
+    
+// working of loops also do same
+
+for(let i=0;i<4;i++){
+    console.log(i)
+}
+// console.log(i)// here it gves the error
+
+
+
+// now see the results of if else condition which gives the same result as we do in up positon
+
+if (true){
+    let nam='ali'
+    console.log(nam)
+}
+else {
+    console.log1(false)
+}
+// console.log(nam) // here it gives the error because call it outside the blocks
