00339dd29374cddc11c670d0356380fe39d7dd22
choudharyumar <choudharyumar948@gmail.com>
2022-12-05 20:12:58 +0500

    Update variableScope_Closure.js

--- Week06/variableScope_Closure.js
+++ Week06/variableScope_Closure.js
@@ -1,31 +1,45 @@
-// Code blocks:
-{
-let mess='heloo'
-console.log(mess)
+// // Code blocks:
+// {
+// let mess='heloo'
+// console.log(mess)
 
-}
-{
-    let mess='heloo'
-    console.log(mess)  // here is the discription of calling inside block than see the results if the value of both variables are same and if we declare outside of the block than there is a error
+// }
+// {
+//     let mess='heloo'
+//     console.log(mess)  // here is the discription of calling inside block than see the results if the value of both variables are same and if we declare outside of the block than there is a error
     
-    }
+//     }
     
-// working of loops also do same
+// // working of loops also do same
 
-for(let i=0;i<4;i++){
-    console.log(i)
-}
-// console.log(i)// here it gves the error
+// for(let i=0;i<4;i++){
+//     console.log(i)
+// }
+// // console.log(i)// here it gves the error
 
 
 
-// now see the results of if else condition which gives the same result as we do in up positon
+// // now see the results of if else condition which gives the same result as we do in up positon
 
-if (true){
-    let nam='ali'
-    console.log(nam)
-}
-else {
-    console.log1(false)
-}
+// if (true){
+//     let nam='ali'
+//     console.log(nam)
+// }
+// else {
+//     console.log1(false)
+// }
 // console.log(nam) // here it gives the error because call it outside the blocks
+
+
+// nested functions:
+
+
+function name(firstname,lastname){
+    function fname(){
+        return firstname+' '+lastname;
+    }
+    console.log('hello',fname())
+    console.log('ali',fname())
+
+}
+console.log(name())
