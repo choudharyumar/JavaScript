7cdd5c2663ba7860af0dd2f781346b04fd263460
choudharyumar <choudharyumar948@gmail.com>
2022-12-07 21:38:19 +0500

    Update variableScope_Closure.js

--- Week06/variableScope_Closure.js
+++ Week06/variableScope_Closure.js
@@ -61,20 +61,121 @@
 //  count();
 
 
- function count()
- {
-  let counter =0;
+//  function count()
+//  {
+//   let counter =0;
   
-    return function newc()
-   {
-        return ++counter;
+//     return function newc()
+//    {
+//         return ++counter;
        
-    }
+//     }
    
     
     
+// }
+// let a=count()
+//  console.log(a());
+
+
+// // Lexical Environment:
+
+
+// let phrase;
+
+// phrase='by';
+
+// phrase='helo';
+// console.log(phrase)
+
+
+// function declaration:
+
+
+// let phrase1='helo';
+// function sam(name)
+// {
+//   console.log(`${phrase1},${name}`)
+// }
+// sam('ali')
+// read from the book to amke it clear more for functons
+
+
+// Garbage collection:
+
+
+// function abc(){
+//   let value=123;
+//   return function(){
+//     return value 
+//   }
+// }
+//  let a=[abc(),abc(),abc()]
+// console.log(a[0]())
+
+
+
+// function f() {
+//   let value = Math.random();
+
+//   function g() {
+//     debugger; // in console: type alert(value); No such variable!
+//   }
+
+//   return g;
+// }
+
+// let ab = f();
+// console.log(ab());
+
+
+// let value = "Surprise!";
+
+// function f() {
+//   let value = "the closest value";
+//   console.log(value)
+
+//   function g() {
+//     debugger; // in console: type alert(value); Surprise!
+//   }
+
+//   return g;
+// }
+// let g = f();
+// g();
+
+
+
+
+
+// function say(fn,ls){
+//   function a(){
+//     return fn+' '+ls;
+//   }
+//    console.log('g' , a())
+// }
+// say('ali','hamza')
+
+
+// function name(firstname,lastname){
+//   function fname(){
+//       return firstname+' '+lastname;
+//   }
+
+//   console.log('g',fname())
+//   console.log('g',fname())
+
+// }
+// name('umar','hayat')
+
+
+
+let name = "John";
+
+function sayHi() {
+  console.log("Hi, " + name);
 }
-let a=count()
- console.log(a());
 
+name = "Pete";
 
+sayHi(); 
