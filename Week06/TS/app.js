7d998bf81a4142c95ba57432d53d411f29cdf283
choudharyumar <choudharyumar948@gmail.com>
2022-12-17 21:33:10 +0500

    ts

--- Week06/TS/app.js
+++ Week06/TS/app.js
@@ -1,17 +1,45 @@
-var myage;
-myage = 16; //narrowing
-console.log(myage);
-myage = 'dont know';
-console.log(myage);
-var newage = Math.random() > 0.6 ? 'khan' : 30;
-if (newage === 'khan') {
-    console.log(newage.toUpperCase());
+"use strict";
+// let myage:string|number;
+// myage=16;//narrowing
+// console.log(myage)
+// myage='dont know';
+// console.log(myage)
+// let newage=Math.random()>0.6?'khan':30;
+// if (newage==='khan')
+// {
+// console.log(
+//     newage.toUpperCase());
+// }
+// let a=5;
+// console.log(typeof a)
+// let age:number|'died'|string;
+// age=90;
+// age='died';
+// age='unknown';
+// age='living';
+// console.log(age)
+// // optional
+// age?.toUpperCase();// thi check if its ok than it returns other wise its optional 
+// class point{
+//     x= 0;
+// y=0;
+// }
+// const pt=new point()
+// pt.x=9;
+// pt.y=7;
+// console.log(`${pt.x},${pt.y}`)
+// class obj{
+//     x:string;
+//     constructor(){
+//         this.x="hello"
+//     }
+// }
+// let obj1 = new obj()
+// console.log(obj1.x)
+class Point {
+    constructor(xs, y) {
+        // TBD
+    }
 }
-var a = 5;
-console.log(typeof a);
-var age;
-age = 90;
-age = 'died';
-age = 'unknown';
-age = 'living';
-console.log(age);
+let obj = new Point(3);
+console.log(obj);
--- Week06/TS/app.ts
+++ Week06/TS/app.ts
@@ -1,26 +1,64 @@
-let myage:string|number;
-myage=16;//narrowing
-console.log(myage)
+// let myage:string|number;
+// myage=16;//narrowing
+// console.log(myage)
 
-myage='dont know';
-console.log(myage)
+// myage='dont know';
+// console.log(myage)
 
-let newage=Math.random()>0.6?'khan':30;
-if (newage==='khan')
-{
-console.log(
-    newage.toUpperCase());
-}
+// let newage=Math.random()>0.6?'khan':30;
+// if (newage==='khan')
+// {
+// console.log(
+//     newage.toUpperCase());
+// }
 
-let a=5;
-console.log(typeof a)
-let age:number|'died'|string;
-age=90;
-age='died';
-age='unknown';
-age='living';
-console.log(age)
+// let a=5;
+// console.log(typeof a)
+// let age:number|'died'|string;
+// age=90;
+// age='died';
+// age='unknown';
+// age='living';
+// console.log(age)
 
 
-// optional
-age?.toUpperCase();// thi check if its ok than it returns other wise its optional 
+// // optional
+// age?.toUpperCase();// thi check if its ok than it returns other wise its optional 
+
+
+// class point{
+
+//     x= 0;
+// y=0;
+
+// }
+// const pt=new point()
+// pt.x=9;
+// pt.y=7;
+// console.log(`${pt.x},${pt.y}`)
+
+
+
+// class obj{
+//     x:string;
+//     constructor(){
+//         this.x="hello"
+//     }
+// }
+// let obj1 = new obj()
+// console.log(obj1.x)
+
+
+
+class Point {
+    // s:string;
+    // Overloads
+    constructor(x: number, y: string);
+    constructor(s: string)
+    constructor(xs: any, y?: any) {
+      super(s)
+    }
+  }
+  let obj=new Point("hello");
+  console.log(obj.s)
+  
