85ff7ea8fee54cadcd15fc5d9c7bd7aa9619c412
choudharyumar <choudharyumar948@gmail.com>
2022-12-19 00:29:25 +0500

    Update function_object.js

--- Week06/function_object.js
+++ Week06/function_object.js
@@ -71,23 +71,170 @@
 
 
 
-function makeCounter(){
-    function counter(){
-        return counter.count++
-    }
-     counter.count=0
-    return counter;
-}
-let counter=makeCounter();
-console.log(counter)
-console.log(counter)
-console.log(counter())
-console.log(counter())
+// function makeCounter(){
+//     function counter(){
+//         return counter.count++
+//     }
+//      counter.count=0
+//     return counter;
+// }
+// let counter=makeCounter();
+// console.log(counter)
+// console.log(counter)
+// console.log(counter())
+// console.log(counter())
+
+
+
+// named function expressions:
+// function h(who){
+//     console.log(`helo  ${who}`)
+// }
+// h("ali");
+
+// same as up code in which we use the NFE:
+
+
+// let f=function func(who){
+//     console.log(`helo ${who}`)
+// }
+// f("ali")
+
+
+// we see it in more details 
+
+// let f=function func(who){
+//     if (who){
+//         console.log(`"helo "${who}`)
+//     }
+//     else{
+//         f("guest")
+//     }
+// }
+// let wel =f
+// f=null
+// wel()     // it gives error
+
+
+
+// now we write a program using nfe
+
+// let f=function qa(who){
+//     console.log(who) 
+        
+    
+//     if (who){
+//         console.log(`"helo "${who}`)
+//     }
+//     else{
+//         qa("guest")
+//     }
+// }
+
+// let wel =f
+//  f=null
+// wel()     
+
+
+
+
+// function f(who){ 
+// if (who){
+//     console.log(`"helo "${who}`)
+// }
+// else{
+//     f("guest")
+// }
+
+// }
+// f=null
+// let wel=f
+// // f=null
+// wel// console.log(wel)
+
+
+
+
+// function makeCounter() {
+//     // instead of:
+//     // let count = 0
+  
+//     function abc() {
+//       return abc.count++;
+//     };
+  
+//     abc.count = 0;
+  
+//     return abc;
+//   }
+  
+//   let counter = makeCounter();
+//    counter.count=10
+//   console.log( counter() ); // 0
+//   console.log( counter() ); // 1
 
 
 
 
 
+// Set and decrease for counter
+
+
+// function makeCounter() {
+//     let count=10
+    
+//     console.log(count)
+  
+//     function abc() {
+//       return count;
+//     };
+//    abc.set=value => count=value
+//    abc.decrease=() => count=--count
+//    return abc
+//   }
+
+//   // console.log(makeCounter)
+//   let counter = makeCounter();
+//   console.log( counter.set(5) ); // 0
+//   console.log( counter.decrease() ); // 1
 
 
 
+
+  // function f(a){
+
+  //   let crnt_sm=a;
+  //   function b(d){
+  //        crnt_sm +=d;
+  //         return b
+  //   };
+  //   b.toString=function(){
+  //     return crnt_sm
+  //   };
+  //   return b
+    
+
+  // }
+
+  // console.log(f(1)(3)(4))
+
+  function sum(a) {
+
+    function b(b) {
+       if(b)
+       {
+        
+           let c=sum(a+b)
+           return c; 
+       }
+       return a
+       
+     }
+     return b;
+   }
+  //  let a=sum(1)(2)
+   // console.log(a()())
+   console.log( sum(1)(2)()); // 3
+   console.log( sum(5)(-1)(2)() ); // 6
+   console.log( sum(6)(-1)(-2)(-3)() ); // 0
+   console.log( sum(0)(1)(2)(3)(4)(5)() ); //
