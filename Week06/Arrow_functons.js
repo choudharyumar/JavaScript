102c08948045fc41502e55199f505b79c0ba3a88
choudharyumar <choudharyumar948@gmail.com>
2023-01-07 20:58:10 +0500

    Update Arrow_functons.js

--- Week06/Arrow_functons.js
+++ Week06/Arrow_functons.js
@@ -1,1 +1,27 @@
-console.log("hello")
+// Arrow functions have no “this”"
+
+// let group={
+//     class:5,
+//     list:["math","science","male"],
+//     show(){
+//         this.list.forEach(
+//             li => console.log(this.class+" : "+li)
+//         )
+//     }
+// }
+// group.show()
+
+
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+  
+    showList() {
+      this.students.forEach(function(student) {
+        // Error: Cannot read property 'title' of undefined
+        console.log(this.title + ': ' + student);
+      });
+    }
+  };
+  
+  group.showList();// in this title = undefined
