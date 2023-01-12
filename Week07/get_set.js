ea90498d3468691cc3b665e6d0b024eba794745d
choudharyumar <choudharyumar948@gmail.com>
2023-01-13 00:02:47 +0500

    h

---
+++ Week07/get_set.js
@@ -0,0 +1,12 @@
+// console.log("umar")
+
+let user={
+    name:"umar",
+    surname:"hayat",
+    get fullname(){
+    //  return `${this.name}${this.surname}`
+         console.log(this.name+this.surname)
+    }
+}
+// console.log(user.fullname)
+user.fullname="ggg"
